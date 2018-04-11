cf219ae0d9ce9c7be3275d1ede07a2157f3b77be
Lavanya-Lvn <38152986+Lavanya-Lvn@users.noreply.github.com>
2018-04-11 10:58:44 +0530

    Add files via upload

---
+++ FirstSpringBoot.java
@@ -0,0 +1,13 @@
+package io.lavanya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBoot {
+
+	public static void main(String[] args) {
+     SpringApplication.run(FirstSpringBoot.class, args);
+	}
+
+}
